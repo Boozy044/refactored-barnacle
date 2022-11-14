a8547ca9d03bdb8882368c52f7fce95710331745
Boozy044 <boozy044@gmail.com>
2022-11-14 20:33:55 +0000

    Additional dependencies for Element plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      dts: fileURLToPath(new URL('./src/types/auto-import.d.ts', import.meta.url))
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: fileURLToPath(new URL('./src/types/components.d.ts', import.meta.url))
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
